ee78ec52be454ecfe10b1f30b3bd6ef960aeb1c9
juniin1233 <washington.juniin2018@gmail.com>
2024-05-13 16:42:23 -0300

    Algumas alterações

--- expenses/lib/homepage.dart
+++ expenses/lib/homepage.dart
@@ -58,13 +58,13 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
+          backgroundColor: Colors.purple,
           actions: [
             IconButton(
               onPressed: () => _openTransactionFormModal(context),
               icon: const Icon(Icons.add),
             ),
           ],
-          backgroundColor: Colors.blue,
           title: const Text("Despesas Pessoais"),
         ),
         body: SingleChildScrollView(
