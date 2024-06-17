390eea979c484b58f3c9e71659d738c14d24b3c3
juniin1233 <washington.juniin2018@gmail.com>
2024-06-17 18:14:06 -0300

    Criando a barra
    Criei a barra que irá subir conforme a porcentagem aumentar!!

--- expenses/lib/widgets/chart.dart
+++ expenses/lib/widgets/chart.dart
@@ -1,4 +1,5 @@
 import 'package:expenses/models/transactions.dart';
+import 'package:expenses/widgets/chart_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -8,7 +9,7 @@ class Chart extends StatelessWidget {
   final List<Transactions> recentTransaction;
 
   // Função criada para agrupar as transições feitas
-  // Essa função retorna uma lista do tipo Map 
+  // Essa função retorna uma lista do tipo Map
   List<Map<String, Object>> get grouepTransactions {
     return List.generate(7, (index) {
       // Variavel para pegar os ultimos 7 dias
@@ -18,13 +19,13 @@ class Chart extends StatelessWidget {
 
       double totalSum = 0.0;
 
-      // Varendo a lista de transações, para ver se as transações foram feitas nos ultimos 7 dias 
-      for(var i = 0; i < recentTransaction.length; i++){
+      // Varendo a lista de transações, para ver se as transações foram feitas nos ultimos 7 dias
+      for (var i = 0; i < recentTransaction.length; i++) {
         bool sameDay = recentTransaction[i].date.day == weekDay.day;
         bool sameMonth = recentTransaction[i].date.month == weekDay.month;
         bool sameYear = recentTransaction[i].date.year == weekDay.year;
 
-        if(sameDay && sameMonth && sameYear){
+        if (sameDay && sameMonth && sameYear) {
           totalSum += recentTransaction[i].value;
         }
       }
@@ -42,11 +43,16 @@ class Chart extends StatelessWidget {
     grouepTransactions;
     return Card(
       elevation: 6,
-      margin:  const EdgeInsets.all(20),
+      margin: const EdgeInsets.all(15),
       child: Row(
         // Mapeando dados para um Widgets
         children: grouepTransactions.map((tr) {
-          return Text(' ${tr['day']} : ${tr['value']}');
+          return ChartBar(
+            // Convertendo uma variavel do tipo objeto para o tipo String/double apenas com o prefixo "as"
+            label: tr['day'] as String,
+            value: tr['value'] as double,
+            percentage: 0.3,
+          );
         }).toList(),
       ),
     );
--- expenses/lib/widgets/chart_bar.dart
+++ expenses/lib/widgets/chart_bar.dart
@@ -1,14 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class ChartBar extends StatelessWidget {
+  const ChartBar({
+    super.key,
+    required this.label,
+    required this.value,
+    required this.percentage,
+  });
 
-  const ChartBar({ super.key });
+  final String label;
+  final double value;
+  final double percentage;
 
-   @override
-   Widget build(BuildContext context) {
-       return Scaffold(
-           appBar: AppBar(title: const Text(''),),
-           body: Container(),
-       );
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text("R\$${value.toStringAsFixed(2)}"),
+        const SizedBox(
+          height: 5,
+        ),
+        // ignore: sized_box_for_whitespace
+        Container(
+          height: 60,
+          width: 10,
+          child: Stack(
+            // Stack um widget que empilha outros widget
+            alignment: AlignmentDirectional.bottomCenter,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.grey,
+                    width: 1.0,
+                  ),
+                  color: const Color.fromRGBO(220, 220, 220, 1),
+                  borderRadius: BorderRadius.circular(5)
+                ),
+              ),
+              // Um SizedBox que a altura/largura aumenta de tamanho conforme a sua porcentagem
+              FractionallySizedBox(
+                // heightFactor: 0.9,
+                heightFactor: percentage,
+                child: Container(
+                  decoration: BoxDecoration(
+                  color: Colors.purple,
+                  borderRadius: BorderRadius.circular(5)
+                ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        const SizedBox(
+          height: 5,
+        ),
+        Text(label)
+      ],
+    );
   }
 }
